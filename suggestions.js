4b65c8821f7aa13114d22c2e5b7570c500890e2f
dasariakhil001 <146342538+dasariakhil001@users.noreply.github.com>
2023-10-08 15:56:26 +0530

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,22 @@
+let suggestions = [
+    "Idly",
+    "Dosa",
+    "Bonda",
+    "Upma",
+    "Butter Chicken",
+    "Chana Masala",
+    "Vegetable Pulao",
+    "Aloo Gobi",
+    "Bajji",
+    "Puffs",
+    "Samosa",
+    "Biscuits",
+    "Vegetable Biryani",
+    "Palak Paneer",
+    "Dal Tadka",
+    "Gulab Jamun",
+    "Rasgulla",
+    "Kheer",
+    "Jalebi",
+    "sugar  -  kheer" ,  "sugar  -  Gulab jamun"
+];
